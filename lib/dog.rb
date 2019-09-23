0a510b0d07151055891be231d559f3a40f0f22f9
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-23 19:01:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
   def self.find_by_id(num)
     sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1;"
     DB[:conn].execute(sql, num).map do |row|
-      Dog.new(row[0], row[1], row[2])
+      Dog.new(id: row[0],name: row[1],breed: row[2])
     end
   end
   
