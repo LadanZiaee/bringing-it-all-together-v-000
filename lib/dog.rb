749fdb7af6a9d91d968ce87b8aa66e12a8a6520f
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-23 19:01:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,7 @@ class Dog
   
   def self.new_from_db(row)
     
-    dogs = Dog.new(row[0], row[1], row[2])
+    dogs = Dog.new(id: row[0],name: row[1],breed: row[2])
   end
   
   def self.find_by_id(num)
